4364ea426b2161081ecf5c004b039d2629fbd665
Ghulam Ahmed <dev.ahmed47@gmail.com>
2025-07-16 23:51:44 +0500

    all done

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {store,persistor} from './redux/store.ts'
+import { store, persistor } from './redux/store.ts'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import ThemeProvider from './components/ThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <PersistGate persistor={persistor}>
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </Provider>
-  </PersistGate>
-);
+    </PersistGate>
+  </Provider>
+)
